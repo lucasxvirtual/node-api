cc1cdcdd3becc4881a10d69415204ef657cf0cc2
unknown <lucasxvirtual@gmail.com>
2020-02-17 02:53:32 -0300

    adding email validator

--- api/users/userService.js
+++ api/users/userService.js
@@ -1,7 +1,18 @@
 const User = require('./user');
+const validator = require("email-validator");
 
 User.methods(['get', 'post', 'put', 'delete']);
 
 User.updateOptions({new: true, runValidators: true});
+
+User.before('post', function(req, res, next){
+    let email = req.params.email
+    if(!validator.validate(email)){
+        res.statusCode = 400;
+        return res.json({ errors: ["Invalid email"] });
+    } else {
+        next()
+    }
+})
 
 module.exports = User
